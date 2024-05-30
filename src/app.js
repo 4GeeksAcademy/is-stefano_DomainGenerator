7b55b274116951a0af190a25d18aebff100b82c0
Stefano Imperial Salvador <105392252+is-stefano@users.noreply.github.com>
2024-05-30 19:30:43 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".es", ".com"];
+
+  function generateDomains(pronoun, adj, noun, dom) {
+    let domains = [];
+
+    for (let p of pronoun) {
+      for (let a of adj) {
+        for (let n of noun) {
+          for (let d of dom) {
+            domains.push(`${p}${a}${n}${d}`);
+          }
+        }
+      }
+    }
+    return domains;
+  }
+
+  let domainNames = generateDomains(pronoun, adj, noun, dom);
+  domainNames.map(domain => {
+    document.querySelector("#generador").innerHTML +=
+      "<li class='list-group-item'>" + domain + "</li>";
+  });
 };
