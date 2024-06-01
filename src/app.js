2e17fcb69d56ece8af61c113041e2b61d7b8a78b
Stefano Imperial Salvador <105392252+is-stefano@users.noreply.github.com>
2024-06-01 10:33:30 +0000

    feat:arreglo

--- src/app.js
+++ src/app.js
@@ -1,33 +1,31 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let dom = [".es", ".com"];
+  // Write your code here
 
-  function generateDomains(pronoun, adj, noun, dom) {
-    let domains = [];
-
+  function generateDomains() {
+    let domainResult = [];
     for (let p of pronoun) {
-      for (let a of adj) {
+      for (let a of adjetive) {
         for (let n of noun) {
-          for (let d of dom) {
-            domains.push(`${p}${a}${n}${d}`);
+          for (let d of domain) {
+            domainResult.push(`${p}${a}${n}${d}`);
           }
         }
       }
     }
-    return domains;
+    return domainResult;
   }
 
-  let domainNames = generateDomains(pronoun, adj, noun, dom);
+  let pronoun = ["the", "our"];
+  let adjetive = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".es", ".com"];
+  let domainNames = generateDomains();
   domainNames.map(domain => {
     document.querySelector("#generador").innerHTML +=
       "<li class='list-group-item'>" + domain + "</li>";
